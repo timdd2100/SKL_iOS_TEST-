099c18321ba8c23573fd7a5727f33174a5384168
timkao <timkao@jason-pc.skl.com.tw>
2013-11-15 08:52:55 +0800

    Initial Commit

---
+++ Chapter3/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Chapter3
+//
+//  Created by timkao on 13/11/15.
+//  Copyright (c) 2013年 skl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
